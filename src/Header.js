149ad60480744debc2b3cfe3bff14fdc122e3eb9
indiesurya <putuindie11@gmail.com>
2022-04-17 21:50:47 +0800

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+// component (rcc)
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+          <h3>Header</h3>
+      </div>
+    )
+  }
+}
---
+++ src/Navbar.js
@@ -0,0 +1,39 @@
+// import React, { Component } from 'react'
+
+// // Class Component (rcc)
+// // export default class Navbar extends Component {
+// //   render() {
+// //     return (
+// //       <div>Navbar</div>
+// //     )
+// //   }
+// // }
+
+//function Component (rfc)
+// import React from 'react'
+
+// export default function Navbar() {
+//   return (
+//     <div>Navbar</div>
+//   )
+// }
+
+
+//Arrow function (rafc)
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <h3>Navbar</h3>
+    </div>
+  )
+}
+export default Navbar
+
+
+
+
+
+
+
